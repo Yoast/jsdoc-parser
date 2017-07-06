bd08e88940f1c68cc674f6078947d8d66c836ba9
Anton Timmermans <email@atimmer.com>
2017-07-06 14:57:31 +0200

    Add the basis for a plugin

---
+++ plugin.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: WP JS Doc Parser
+ * Description: Create a function reference site powered by WordPress
+ * Author: Team Yoast
+ * Author URI: https://github.com/Yoast/jsdoc-parser/graphs/contributors
+ * Plugin URI: https://github.com/Yoast/jsdoc-parser
+ * Version: 0.1.0
+ * Text Domain: wp-js-parser
+ */
+
+if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+	require __DIR__ . '/vendor/autoload.php';
+}
+
+
