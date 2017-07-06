dd90f2408b5b328b6c9c7c86627994a29e5d69a9
Anton Timmermans <email@atimmer.com>
2017-07-06 13:36:32 +0200

    Add basic script to parse WordPress

---
+++ create-docs.js
@@ -0,0 +1,43 @@
+const glob = require( "glob" );
+
+const ignoredFiles = [
+	"Gruntfile.js",
+	"create-docs.js",
+	"src/wp-includes/js/media-views.js",
+	"src/wp-includes/js/media-audiovideo.js",
+	"src/wp-includes/js/media-grid.js",
+	"src/wp-includes/js/media-models.js",
+	
+	// Add these back later:
+	"src/wp-includes/js/tinymce/tinymce.js",
+	"src/wp-includes/js/customize-selective-refresh.js",
+	"src/wp-includes/js/wp-api.js",
+	"src/wp-admin/js/updates.js",
+	"src/wp-admin/js/customize-controls.js",
+];
+
+let files = glob( "**/*.js", {}, ( err, files ) => {
+	files = files.filter( ( file ) => {
+		return ! file.endsWith( ".min.js" );
+	} );
+	
+	files = files.filter( ( file ) => {
+		return ! file.startsWith( "out" );
+	} );
+	
+	files = files.filter( ( file ) => {
+		return ! file.startsWith( "tests" );
+	} );
+
+	files = files.filter( ( file ) => {
+		return ! ignoredFiles.includes( file );
+	} );
+
+	files = files.filter( ( file ) => {
+		return ! file.startsWith( "src/wp-includes/js/jquery" );
+	} );
+
+	files = files.join( " " );
+	
+	console.log( `documentation build ${files} -f html -o documentation` );
+} );
