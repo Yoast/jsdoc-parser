e4caafe3358bc4fd8394d82883927d660a12968c
Jimmy Comack <jimmy@yoast.com>
2017-07-27 14:45:55 +0200

    Initial commit

--- create-docs.js
+++ create-docs.js
@@ -1,4 +1,8 @@
 const glob = require( "glob" );
+const exec = require( "child_process").exec;
+const documentation = require( "documentation" );
+const streamArray = require('stream-array');
+const vfs = require('vinyl-fs');
 
 const ignoredFiles = [
 	"Gruntfile.js",
@@ -16,28 +20,87 @@ const ignoredFiles = [
 	"src/wp-admin/js/customize-controls.js",
 ];
 
-let files = glob( "**/*.js", {}, ( err, files ) => {
-	files = files.filter( ( file ) => {
-		return ! file.endsWith( ".min.js" );
+function filterIgnoredFiles( files, startingDirectory ) {
+	return files.filter( ( file ) => {
+
+		if ( file.startsWith( `${startingDirectory}/out` ) ) {
+			return false;
+		}
+
+		if ( file.startsWith( `${startingDirectory}/tests` ) ) {
+			return false;
+		}
+
+		if ( file.startsWith( `${startingDirectory}/wp-includes/js/jquery` ) ) {
+			return false;
+		}
+
+		if ( file.endsWith( `.min.js` ) ) {
+			return false;
+		}
+
+		return true;
 	} );
-	
-	files = files.filter( ( file ) => {
-		return ! file.startsWith( "out" );
-	} );
-	
-	files = files.filter( ( file ) => {
-		return ! file.startsWith( "tests" );
+};
+
+function output_to_file( content, destination ) {
+	content.then( ( result ) => {
+		let outputted = streamArray( result ).pipe( vfs.dest( destination ) );
+
+		console.log( "Outputted to: ", destination );
+
+		return outputted;
+	} ).catch( ( err ) => {
+		console.log( err );
+		return;
+	});
+}
+
+function output_documentation( files, destination = "./documentation" ) {
+	let errors = [];
+
+	let resultOutput = {
+		errors: [],
+		results: null,
+	};
+
+	return documentation
+		.build( files, {} )
+//		.catch( error => { errors.push( error ); return; } )
+	    .then( result => {
+	    	console.log( "hi" );
+	    	resultOutput.results = documentation.formats.html( result, {} )
+	    } )
+	    .catch( error => { errors.push( error ); return; } )
+		.then( results => resultOutput );
+}
+
+let startingDirectory = process.argv[2].replace(/\/$/, "");
+
+let files = glob( `${startingDirectory}/**/*.js`, {}, ( err, files ) => {
+	files = filterIgnoredFiles( files, startingDirectory );
+
+	let l_files = files.slice( 0,  );
+	let completed = 0;
+
+//	for ( let file in l_files ) {
+	let output = output_documentation( files[0] );
+
+	output.then( output => {
+
+		console.log( output );
+
+
+		output_to_file( output.results, "./documentation2" )
+	} ).catch( ( err ) => {
+		console.log( err );
+
+		return;
 	} );
 
-	files = files.filter( ( file ) => {
-		return ! ignoredFiles.includes( file );
-	} );
+//		completed++;
+//	}
+//
+//	console.log( completed )
 
-	files = files.filter( ( file ) => {
-		return ! file.startsWith( "src/wp-includes/js/jquery" );
-	} );
-
-	files = files.join( " " );
-	
-	console.log( `documentation build ${files} -f html -o documentation` );
 } );
